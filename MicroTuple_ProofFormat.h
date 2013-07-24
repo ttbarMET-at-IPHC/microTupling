c835d67374dbf823dc6dafc69170c60a8fe4b56b
Alexandre Aubin <alexandre.aubin@cern.ch>
2013-07-24 12:36:16 +0200

    Fixing ProofFormat double include

--- MicroTuple_ProofFormat.h
+++ MicroTuple_ProofFormat.h
@@ -5,7 +5,7 @@
 // Here, choose the format version you want to use
 
 //#include "MicroTuple_Format_WTag0430.h"
-#include "MicroTuple_Format_WTag0719.h"
+//#include "MicroTuple_Format_WTag0719.h"
 //#include "MicroTuple_Format_Synchro0716.h"
 //#include "MicroTuple_Format_Synchro0722.h"
 #include "MicroTuple_Format_MVANoWTag0723.h"
