11d2717cf1e3076c98acaa826925d177062fbca2
Alexandre Aubin <alexandre.aubin@cern.ch>
2013-09-27 12:38:19 +0200

    Editing options for proof jobs

--- MicroTuple_ProofLauncher.h
+++ MicroTuple_ProofLauncher.h
@@ -3,7 +3,7 @@
 #define ProofMicroTupler_h
 
 // Number of nodes to be used
-#define NUMBER_OF_NODES 10
+#define NUMBER_OF_NODES 16
 
 // Name of the macro to be run
 #define MACRO_NAME "MicroTuple_ProofJob.C+"
@@ -15,6 +15,6 @@
 #define OUTPUT_BOX "/opt/sbg/data/data4/cms/aaubin/proofBox/"
 
 // Name for the final output
-#define OUTPUT_NAME "MicroTuple_output.root"
+#define OUTPUT_NAME "out.root"
 
 #endif
